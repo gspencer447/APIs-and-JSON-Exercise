a3ae22c7b0d1c14219b343bf406beb543cb2728b
Graham Spencer <gspencer447@yahoo.com>
2023-09-27 10:37:49 -0500

    completed all portions of exercise 2

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,24 +1,19 @@
 ﻿using Newtonsoft.Json.Linq;
+using System.Runtime.CompilerServices;
+using System;
 
 namespace APIsAndJSON
 {
     public class Program
     {
+            private const string ApiKey = "6ba521e04dcca23e89b903aa5087600d";
         static void Main(string[] args)
         {
             HttpClient client = new HttpClient();
+
+            #region
             string ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
             string kanyeURL = "https://api.kanye.rest/";
-            string weatherURL = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={6ba521e04dcca23e89b903aa5087600d}&units=imperial";
-
-            var weatherResponse = client.GetStringAsync(weatherURL);
-            JObject weatherObject = JObject.(weatherResponse);
-
-            Console.WriteLine("Please enter the name of the city you with to display the weather for.");
-            var userInput = Console.ReadLine();
-
-            Console.WriteLine($"The current temperature in {userInput} is {weatherObject} degrees fahrenheit.");
-
 
             for (var item = 0; item < 5; item++)
             {
@@ -35,7 +30,28 @@ namespace APIsAndJSON
                     Console.WriteLine("Kanye West: " + kanyeObject["quote"]);
                 }
             }
+            #endregion
 
+            Console.WriteLine("Please enter the name of the city you with to display the weather for.");
+            var userInput = Console.ReadLine();
+
+            string weatherURL = $"https://api.openweathermap.org/data/2.5/weather?q={userInput}&appid={ApiKey}&units=imperial";
+
+            try
+            {
+                var weatherResponse = client.GetStringAsync(weatherURL).Result;
+                JObject weatherObject = JObject.Parse(weatherResponse);
+
+                double temperature = (double)weatherObject["main"]["temp"];
+
+
+                Console.WriteLine($"The current temperature in {userInput} is {temperature} degrees fahrenheit.");
+
+            }
+            catch (HttpRequestException error)
+            {
+                Console.WriteLine($"Error: {error.Message}");
+            }
         }
     }
 }
